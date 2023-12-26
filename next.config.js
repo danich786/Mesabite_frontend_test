96d779467893f620273f2e34ff211a9a1507f8ba
Danish <dani@macbooks-MacBook-Pro.local>
2023-12-26 06:14:18 +0500

    removed redundancies

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // env: {
-  //   NEXT_PUBLIC_HOST: "https://p5ijytb2fm.eu-west-3.awsapprunner.com",
-  // },
+  env: {
+    NEXT_PUBLIC_HOST: "https://p5ijytb2fm.eu-west-3.awsapprunner.com",
+  },
 };
 
 module.exports = nextConfig;
